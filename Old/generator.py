7d15c4862609285bd833ca80684e695440c49813
Alexandr Garberov <garberov@gmail.com>
2020-08-19 16:50:14 +0500

    ver11

---
+++ Old/generator.py
@@ -0,0 +1,25 @@
+import random
+
+def fib():
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b, a+b
+
+#x=fib()
+#print (next(x))
+
+def  task1():
+    a1 = random.randint(0,100)
+    while True:
+        yield a1
+        a1 = random.randint(0,100)
+
+y=task1()
+print(next(y))
+print(next(y))
+print(next(y))
+print(next(y))
+print(next(y))
+print(list(y))
+
